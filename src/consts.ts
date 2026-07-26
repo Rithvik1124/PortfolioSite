9445c88d8aed0797a8269bb5aaa2ddce081fb08f
Rithvik1124 <pvrithvik456@gmail.com>
2026-07-26 15:54:53 +0530

    Remove accidental submodule

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+//https://in.pinterest.com/pin/810507264235890620/
+//https://in.pinterest.com/pin/769693392609451767/
